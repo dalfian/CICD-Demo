b5936a2ac2ad2c5b22d30c17c934cd7d1b5bf9b3
Jie Wu <jiewu97@gmail.com>
2020-08-24 20:33:16 -0400

    Minor fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           CICD-Demo
         </a>
       </header>
